8cc1ff80ecfaf80e552feeb40d222ee36bb8d060
Eder Sena <eder.sena@live.com>
2023-08-11 16:59:20 -0300

    Adicionando Imagem

--- app/src/main/java/br/com/fiap/imcfiap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/imcfiap/MainActivity.kt
@@ -3,17 +3,27 @@ package br.com.fiap.imcfiap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fiap.imcfiap.ui.theme.IMCFiapTheme
@@ -43,10 +53,21 @@ fun IMCScreen() {
             // ######## Header ########
             // Column 2
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(160.dp),
                 ) {
+                //Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription = "Biking",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(top = 16.dp),
+                        //.clip(shape = RoundedCornerShape(8.dp)),
+                    //contentScale = ContentScale.Crop
+                )
             }
             // ######## Formulario ########
             // Column 3
