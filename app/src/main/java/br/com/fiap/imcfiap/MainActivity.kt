18a2856b6eb96c5dabfff6dd31ebea3f9e3b52bd
Eder Sena <eder.sena@live.com>
2023-08-11 17:52:52 -0300

    Adicionando cores custom e usando

--- app/src/main/java/br/com/fiap/imcfiap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/imcfiap/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -23,9 +24,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.imcfiap.ui.theme.IMCFiapTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +60,8 @@ fun IMCScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(160.dp),
+                    .height(160.dp)
+                    .background(colorResource(id = R.color.vermelho_fiap)),
                 ) {
                 //Spacer(modifier = Modifier.height(16.dp))
                 Image(
@@ -68,15 +73,25 @@ fun IMCScreen() {
                         //.clip(shape = RoundedCornerShape(8.dp)),
                     //contentScale = ContentScale.Crop
                 )
+                // Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "CALCULADORA IMC",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(id = R.color.cor_do_texto),
+                    modifier = Modifier.padding(top = 12.dp, bottom = 24.dp)
+                )
             }
             // ######## Formulario ########
             // Column 3
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 32.dp)
             ) {
                 // Card Formulario
+
             }
             // ######## Card Resultado ########
 
